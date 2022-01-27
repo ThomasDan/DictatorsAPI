915ea0129f586e6dd7d67d649d128441ccb9b1cc
Thomas Carl Alexander Dänhardt <thomas.danhardt@gmail.com>
2022-01-27 20:34:43 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+
+
+
+// use "node app" command to launch
+
+// https://malcoded.com/posts/angular-backend-express/
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+
+let dictators = [];
+
+app.get('/getDictators', function(req, res) {
+    console.log(dictators);
+    res.send(dictators);
+});
+
+app.post('/create', function (req, res) {
+    const dictator = req.body;
+    console.log('Attempted to create a dictator!');
+    
+    dictators.push(dictator);
+  });
+
+app.delete('/delete', function (req, res) { 
+    const dictator = req.body.Index;
+    dictators.splice(dictator, 1);
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
